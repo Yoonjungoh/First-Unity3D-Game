096e7a28aa58a98be1af4ac68de2f8b28bdd75b3
Yoonjungoh <arcnature@naver.com>
2022-03-20 15:03:06 +0900

    MyFirst Unity Upload

---
+++ Assets/Scripts/Contents/SpawningPool.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawningPool : MonoBehaviour
+{
+    [SerializeField]
+    int _monsterCount = 0;
+    int _reserveCount = 0;
+
+    [SerializeField]
+    int _keepMonsterCount = 0;
+
+    [SerializeField]
+    Vector3 _spawnPos;
+    [SerializeField]
+    float _spawnRadius = 15.0f;
+    [SerializeField]
+    float _spawnTime = 5.0f;
+
+    public void AddMonsterCount(int value) { _monsterCount += value; }
+    public void SetKeepMonsterCount(int count) { _keepMonsterCount = count; }
+
+    void Start()
+    {
+        Managers.Game.OnSpawnEvent -= AddMonsterCount;
+        Managers.Game.OnSpawnEvent += AddMonsterCount;
+    }
+
+    void Update()
+    {
+        while (_reserveCount + _monsterCount < _keepMonsterCount)
+        {
+            StartCoroutine("ReserveSpawn");
+        }
+    }
+
+    IEnumerator ReserveSpawn()
+    {
+        _reserveCount++;    // 들어 왔으니 일단 예약 이작업 안하면 무한 생성임
+        yield return new WaitForSeconds(Random.Range(0, _spawnTime)); // 일단 0초에서 5초 기다리고 다음 함수 실행
+        GameObject obj = Managers.Game.Spawn(Define.WorldObject.Monster, "Knight");
+        //GameObject obj1 = Managers.Game.Spawn(Define.WorldObject.Monster, "Zombie");
+        NavMeshAgent nma = obj.GetOrAddComponent<NavMeshAgent>();
+        //NavMeshAgent nma1 = obj.GetOrAddComponent<NavMeshAgent>();
+
+        Vector3 randPos;
+        while (true)
+        {
+            Vector3 randDir = Random.insideUnitSphere * Random.Range(0, _spawnRadius);
+			randDir.y = 0;
+			randPos = _spawnPos + randDir;
+
+            // 갈 수 있나
+            NavMeshPath path = new NavMeshPath();
+            if (nma.CalculatePath(randPos, path))
+                break;
+		}
+
+        obj.transform.position = randPos;
+        _reserveCount--;    // 예약 취소
+    }
+}
